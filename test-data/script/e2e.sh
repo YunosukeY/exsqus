27c55ef10e38795ecc3c5cb527cfb045e32c7683
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2023-06-07 00:15:15 +0900

    add e2e

---
+++ test-data/script/e2e.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -eu
+
+docker compose up -d
+mysql --protocol=tcp -h localhost -P 3306 -u root -proot -e "SELECT SLEEP(2);"
+logs="$(docker logs app 2>&1)"
+
+query='"Query":"SELECT SLEEP(2);"'
+if [[ "$logs" == *"$query"* ]]; then
+  echo "query: ok"
+else
+  docker compose down
+  exit 1
+fi
+
+plan='"plan":[{"Id":1,"SelectType":{"String":"SIMPLE","Valid":true},"Table":{"String":"","Valid":false},"Partitions":{"String":"","Valid":false},"AccessType":{"String":"","Valid":false},"PossibleKeys":{"String":"","Valid":false},"Key":{"String":"","Valid":false},"KeyLen":{"String":"","Valid":false},"Ref":{"String":"","Valid":false},"Rows":{"String":"","Valid":false},"Filtered":{"String":"","Valid":false},"Extra":{"String":"No tables used","Valid":true}}]'
+if [[ "$logs" == *"$plan"* ]]; then
+  echo "plan: ok"
+else
+  docker compose down
+  exit 1
+fi
+
+docker compose down
