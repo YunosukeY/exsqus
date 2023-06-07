56d18bea3a45330959ae3a6c3ec9d7f103a35d8f
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2023-06-08 00:18:05 +0900

    add test

--- internal/util/fileutil_test.go
+++ internal/util/fileutil_test.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"bufio"
+	"io"
 	"strings"
 	"testing"
 
@@ -9,6 +10,21 @@ import (
 )
 
 func TestGetLastQueryLog(t *testing.T) {
+	test := `# Time: 2023-06-07T11:59:05.164053Z
+# User@Host: root[root] @  [192.168.16.1]  Id:    12
+# Query_time: 2.001390  Lock_time: 0.000000 Rows_sent: 1  Rows_examined: 1
+SET timestamp=1686139143;
+SELECT SLEEP(2);
+# This row will be skipped.
+`
+	reader := bufio.NewReader(strings.NewReader(test))
+
+	log, err := GetLastQueryLog(reader)
+	assert.Nil(t, err)
+	assert.Equal(t, &Log{Time: "2023-06-07T11:59:05.164053Z", QueryTime: "2.001390", LockTime: "0.000000", RowsSent: "1", RowsExamined: "1", Query: "SELECT SLEEP(2);"}, log)
+
+	_, err = reader.ReadString('\n')
+	assert.Equal(t, io.EOF, err)
 }
 
 func TestGetTime(t *testing.T) {
@@ -21,7 +37,22 @@ func TestGetTime(t *testing.T) {
 }
 
 func TestGetQueryTime(t *testing.T) {
+	test := "# Query_time: 2.001390  Lock_time: 0.000000 Rows_sent: 1  Rows_examined: 1\n"
+	reader := bufio.NewReader(strings.NewReader(test))
+
+	queryTime, lockTime, rowsSent, rowsExamined, err := getQueryTime(reader)
+	assert.Nil(t, err)
+	assert.Equal(t, "2.001390", queryTime)
+	assert.Equal(t, "0.000000", lockTime)
+	assert.Equal(t, "1", rowsSent)
+	assert.Equal(t, "1", rowsExamined)
 }
 
 func TestGetQuery(t *testing.T) {
+	test := "SELECT SLEEP(2);\n"
+	reader := bufio.NewReader(strings.NewReader(test))
+
+	time, err := getQuery(reader)
+	assert.Nil(t, err)
+	assert.Equal(t, "SELECT SLEEP(2);", time)
 }
