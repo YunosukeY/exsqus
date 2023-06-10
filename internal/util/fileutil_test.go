f8817e1db2d9bddaefd440bacc953676b96ba74a
Yunosuke Yamada <39757050+YunosukeY@users.noreply.github.com>
2023-06-10 21:49:51 +0900

    fix: Queries contain line breaks (#34)
    * fix: クエリが改行を含む場合に対応
    
    * fix: --log-short-format対応
    
    * fix

--- internal/util/fileutil_test.go
+++ internal/util/fileutil_test.go
@@ -14,33 +14,23 @@ func TestGetLastQueryLog(t *testing.T) {
 # User@Host: root[root] @  [192.168.16.1]  Id:    12
 # Query_time: 2.001390  Lock_time: 0.000000 Rows_sent: 1  Rows_examined: 1
 SET timestamp=1686139143;
-SELECT SLEEP(2);
-# This row will be skipped.
+SELECT
+SLEEP(2);
 `
 	reader := bufio.NewReader(strings.NewReader(test))
 
 	log, err := GetLastQueryLog(reader)
 	assert.Nil(t, err)
-	assertEqual(t, &Log{Time: "2023-06-07T11:59:05.164053Z", QueryTime: "2.001390", LockTime: "0.000000", RowsSent: "1", RowsExamined: "1", Query: "SELECT SLEEP(2);"}, log)
+	assertEqual(t, &Log{QueryTime: "2.001390", LockTime: "0.000000", RowsSent: "1", RowsExamined: "1", Query: "SELECT SLEEP(2);"}, log)
 
 	_, err = reader.ReadString('\n')
 	assertEqual(t, io.EOF, err)
 }
 
-func TestGetTime(t *testing.T) {
-	test := "# Time: 2023-06-07T11:58:58.688716Z\n"
-	scanner := bufio.NewScanner(strings.NewReader(test))
-
-	time, err := getTime(scanner)
-	assert.Nil(t, err)
-	assertEqual(t, "2023-06-07T11:58:58.688716Z", time)
-}
-
 func TestGetQueryTime(t *testing.T) {
-	test := "# Query_time: 2.001390  Lock_time: 0.000000 Rows_sent: 1  Rows_examined: 1\n"
-	scanner := bufio.NewScanner(strings.NewReader(test))
+	test := "# Query_time: 2.001390  Lock_time: 0.000000 Rows_sent: 1  Rows_examined: 1"
 
-	queryTime, lockTime, rowsSent, rowsExamined, err := getQueryTime(scanner)
+	queryTime, lockTime, rowsSent, rowsExamined, err := getExecInfo(test)
 	assert.Nil(t, err)
 	assertEqual(t, "2.001390", queryTime)
 	assertEqual(t, "0.000000", lockTime)
