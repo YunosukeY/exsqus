08f4123e18504fcc0b484d66ca3b7ed51f32722d
Yunosuke Yamada <39757050+YunosukeY@users.noreply.github.com>
2023-06-10 09:00:58 +0900

    chore: Semantic release (#24)

---
+++ script/build-and-push.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -eu
+
+function split_version() {
+  local version="$1"
+  local vs=(${version//./ })
+  major=${vs[0]}
+  minor=${vs[1]}
+  patch=${vs[2]}
+}
+
+split_version $1
+tags=("latest" "${major}" "${major}.${minor}" "${major}.${minor}.${patch}")
+
+docker login -u kimitsu -p ${DOCKERHUB_PASSWORD}
+
+for t in "${tags[@]}"
+do
+  echo "$t"
+  docker build -t "kimitsu/exsqus:${t}" .
+  docker push "kimitsu/exsqus:${t}"
+done
